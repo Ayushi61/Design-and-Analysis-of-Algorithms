5571692a1245e412fa1b411a011c6c89e729e9fe
Ayushi Rajendra Kumar <arajend4@ncsu.edu>
2020-01-14 12:32:54 -0500

    bubble sort function

---
+++ hw2/bubble_sort.py
@@ -0,0 +1,13 @@
+class sort:
+    def __init__(self):
+        print("in constructor\n")
+
+    def bubble_sort(self,dataset,timestamp_list,size):
+        for i in range(size):
+            for j in range(0,size-i-1):
+                if timestamp_list[j]>timestamp_list[j+1]:
+                    timestamp_list[j],timestamp_list[j+1]=timestamp_list[j+1],timestamp_list[j]
+                    dataset[j],dataset[j+1]=dataset[j+1],dataset[j]
+        return dataset
+
+    def quickSort(self):
