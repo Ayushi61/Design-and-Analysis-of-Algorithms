55eeab2653c8e97fe9a8c894ef9f1204ed9bdaa2
mpnaredi <mpnaredi@ncsu.edu>
2020-01-17 09:43:09 -0500

    need to test it for complete data

--- hw2/bubble_sort.py
+++ hw2/bubble_sort.py
@@ -12,6 +12,51 @@ class sort:
                     dataset[j],dataset[j+1]=dataset[j+1],dataset[j]
         return dataset
 
+    def merge(self,dataset, timestamp_list, l, m, r):
+        n1 = m - l + 1
+        n2 = r - m
+        L = [0] * n1
+        R = [0] * n2
+
+        for i in range(0, n1):
+            L[i] = timestamp_list[l + i]
+            L2[i] = dataset[l + i]
+
+        for j in range(0, n2):
+            R[j] = timestamp_list[m + 1 + j]
+            R2[j] = dataset[m + 1 + j]
+
+        i = 0
+        j = 0
+        k = l
+
+        while i < n1 and j < n2:
+            if L[i] <= R[j]:
+                dataset[k] = L2[i]
+                i += 1
+            else:
+                dataset[k] = R2[j]
+                j += 1
+            k += 1
+
+        while i < n1:
+            dataset[k] = L2[i]
+            i += 1
+            k += 1
+
+        while j < n2:
+            dataset[k] = R2[j]
+            j += 1
+            k += 1
+
+    def mergeSort(self,dataset, timestamp_list, l, r):
+        if l < r:
+            m = (l + (r - 1)) // 2
+
+            self.mergeSort(dataset, timestamp_list, l, m)
+            self.mergeSort(dataset, timestamp_list, m + 1, r)
+            self.merge(dataset, timestamp_list, l, m, r)
+
 
 x=sort()
 
