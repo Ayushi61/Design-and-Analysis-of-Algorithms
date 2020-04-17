e94d1cff9a4d5b4dcd2a6773ff1b9ea55f2108e7
Ayushi Rajendra Kumar <arajend4@ncsu.edu>
2020-04-17 10:25:44 -0400

    q1  sha1_512

--- hw5/hash_func.py
+++ hw5/hash_func.py
@@ -3,7 +3,7 @@ reference: https://www.pythoncentral.io/hashing-strings-with-python/
 '''
 import hashlib
 def hash10(word):
-    obj=hashlib.md5(word.encode())
+    obj=hashlib.sha3_512(word.encode())
     print(int(obj.hexdigest(),16)%1024)
 
 hash10("Ayushi")
