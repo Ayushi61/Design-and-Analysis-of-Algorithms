f8c6bd0dcaa7bae2f9b43cb449165e03e76df285
Ayushi Rajendra Kumar <arajend4@ncsu.edu>
2020-04-18 13:47:52 -0400

    final part3

--- hw5/diff.py
+++ hw5/diff.py
@@ -1,3 +1,6 @@
+''' reference
+https://alex.dzyoba.com/blog/writing-diff/
+'''
 ##!/usr/bin/python3
 from hashfile import hashfile
 import sys
@@ -16,24 +19,31 @@ def print_diff(c, x, y, i, j):
         return ""
     elif i < 0 and j >=0:
         print_diff(c, x, y, i, j - 1)
-        _print("+ " + y[j])
+        print("+ " + y[j].strip("\n"))
     elif j < 0 and i >=0:
         print_diff(c, x, y, i - 1, j)
-        print("- " + x[i])
+        print("- " + x[i].strip("\n"))
     elif x[i] == y[j]:
         print_diff(c, x, y, i - 1, j - 1)
-        print("  " + x[i])
+        print("  " + x[i].strip("\n"))
     elif c[i][j - 1] >= c[i - 1][j]:
         print_diff(c, x, y, i, j - 1)
-        print("+ " + y[j])
+        print("+ " + y[j].strip("\n"))
     elif c[i][j - 1] < c[i - 1][j]:
         print_diff(c, x, y, i - 1, j)
-        print("- " + x[i])
+        print("- " + x[i].strip("\n"))
 
 def diff(x, y,str1,str2):
     c = lcslen(x, y)
     #print(c)
+    print("question part d bonus results")
     return print_diff(c, str1, str2, len(x)-1, len(y)-1)
+
+def diff2(x, y):
+    c = lcslen(x, y)
+    #print(c)
+    print("question part c results")
+    return print_diff(c, x, y, len(x)-1, len(y)-1)
 
 def usage():
     print("Usage: {} <file1> <file2>".format(sys.argv[0]))
@@ -43,4 +53,5 @@ f1,str1=hashfile(sys.argv[1])
 f2,str2=hashfile(sys.argv[2])
 #print(f1)
 #print(f2)
+diff2(f1,f2)
 diff(f1,f2,str1,str2)
