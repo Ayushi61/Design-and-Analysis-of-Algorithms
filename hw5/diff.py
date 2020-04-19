9eccc17bac9e53b5d1ee85a8f5664275187c9ccc
Ayushi Rajendra Kumar <arajend4@ncsu.edu>
2020-04-19 16:52:08 -0400

    final part3

--- hw5/diff.py
+++ hw5/diff.py
@@ -10,26 +10,26 @@ from lcs import lcslen
 
 arr1=[]
 arr2=[]
-def print_diff(c, x, y, i, j):
+def print_diff(c, x, y,str1,str2, i, j):
     """Print the diff using LCS length matrix by backtracking it"""
 
     if i < 0 and j < 0:
         return ""
     elif i < 0 and j >=0:
-        print_diff(c, x, y, i, j - 1)
-        print("+ " + y[j].strip("\n"))
+        print_diff(c, x, y,str1,str2, i, j - 1)
+        print("+ " + str2[j].strip("\n"))
     elif j < 0 and i >=0:
-        print_diff(c, x, y, i - 1, j)
-        print("- " + x[i].strip("\n"))
+        print_diff(c, x, y,str1,str2, i - 1, j)
+        print("- " + str1[i].strip("\n"))
     elif x[i] == y[j]:
-        print_diff(c, x, y, i - 1, j - 1)
-        print("  " + x[i].strip("\n"))
+        print_diff(c, x, y,str1,str2, i - 1, j - 1)
+        print("  " + str1[i].strip("\n"))
     elif c[i][j - 1] >= c[i - 1][j]:
-        print_diff(c, x, y, i, j - 1)
-        print("+ " + y[j].strip("\n"))
+        print_diff(c, x, y,str1,str2, i, j - 1)
+        print("+ " + str2[j].strip("\n"))
     elif c[i][j - 1] < c[i - 1][j]:
-        print_diff(c, x, y, i - 1, j)
-        print("- " + x[i].strip("\n"))
+        print_diff(c, x, y,str1,str2, i - 1, j)
+        print("- " + str1[i].strip("\n"))
 
 def print_diff1(c, x, y, i, j):
 
@@ -57,7 +57,7 @@ def diff(x, y,str1,str2):
     #print(c)
     print("question part d bonus results")
 
-    return print_diff(c, str1, str2, len(x)-1, len(y)-1)
+    return print_diff(c,x, y, str1, str2, len(x)-1, len(y)-1)
 
 def diff2(x, y):
     c = lcslen(x, y)
